5bf88bc3632432e6aa3b3f668a7cf6d7b13a3971
Yamasaki.B <bunta.yamasaki@epkotsoftware.co.jp>
2025-09-22 11:31:53 +0900

    migrationファイル作成完了

---
+++ backend/database/migrations/2025_09_22_013800_create_room_user_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('room_user', function (Blueprint $table) {
+            $table->unsignedBigInteger('room_id');
+            $table->unsignedBigInteger('user_id');
+            $table->unsignedBigInteger('last_read_message_id')->nullable();
+            $table->primary(['room_id', 'user_id']);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('room_user');
+    }
+};
